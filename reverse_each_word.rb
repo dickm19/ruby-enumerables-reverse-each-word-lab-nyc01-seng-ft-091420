881fb20d3f276dcdd08e337e9b0cb6fc607b227e
Mary Dick <maryclairedick@gmail.com>
2020-08-24 20:30:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ require 'pry'
 def reverse_each_word(string)
 
   array = string.split
-  
+  new_array = []
   array.each do |element|
-    new_array = []
     element = element.reverse
     new_array << element
   end
